e7391ab82f27e8893aaa039527bb5b8cc78a4f68
zobugak <zozgdl@gmail.com>
2024-07-08 20:54:45 +0900

    - 레스토랑 스크린 공통 ListView 작업 (대박쓰)

---
+++ lib/common/component/pagination_list_view.dart
@@ -0,0 +1,129 @@
+
+
+// @author zosu
+// @since 2024-07-08
+// @comment Pagination List 공통화
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:zodal_minzok/common/model/cursor_pagination_model.dart';
+import 'package:zodal_minzok/common/model/model_with_id.dart';
+import 'package:zodal_minzok/common/provider/pagination_provider.dart';
+import 'package:zodal_minzok/common/utils/pagination_utils.dart';
+
+
+typedef PaginationWidgetBuilder<T extends IModelWithId> = Widget Function(BuildContext context, int index, T model);
+
+class PaginationListView<T extends IModelWithId> extends ConsumerStatefulWidget {
+  final StateNotifierProvider<PaginationProvider, CursorPaginationBase> provider;
+  final PaginationWidgetBuilder<T> itemBuilder;
+
+  const PaginationListView({required this.provider, required this.itemBuilder, super.key});
+
+  @override
+  ConsumerState<PaginationListView> createState() => _PaginationListViewState<T>();
+}
+
+class _PaginationListViewState<T extends IModelWithId> extends ConsumerState<PaginationListView> {
+  final ScrollController controller = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller.addListener(listener);
+  }
+
+  void listener (){
+    // 스크롤 기능 공통화
+    PaginationUtils.paginate(controller: controller,
+      provider: ref.read(widget.provider.notifier),);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.removeListener(listener);
+    controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final state = ref.watch(widget.provider);
+
+    // 맨 처음 로딩할 때
+    if (state is CursorPaginationLoading) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+
+    // 에러가 발생
+    if (state is CursorPaginationError) {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(state.error, textAlign: TextAlign.center,),
+          const SizedBox(
+            height: 16.0,
+          ),
+          ElevatedButton(onPressed: (){
+            /// 에러 발생 시 다시 paginate 호출 하도록
+            ref.read(widget.provider.notifier).paginate(
+              forceRefetch: true
+            );
+          }, child: Text('다시시도'))
+        ],
+      );
+    }
+
+
+    // 아래 세가지는 모두 CursorPagination
+    // CursorPagination
+    // CursorPaginationFetchingMore
+    // CursorPaginationRefetch
+
+    // 임시
+    final cp = state as CursorPagination;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 16.0,
+      ),
+      child: ListView.separated(
+          controller: controller,
+          itemBuilder: (_, index) {
+            /// 데이터를 더 조회하고 있을 시 로딩
+            if (index == cp.data.length) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16.0,
+                ),
+                child: cp is CursorPaginationFetchingMore /// 데이터가 더이상 없을때 로딩창을 보여주면 안됨
+                    ? const Center(
+                  child: CircularProgressIndicator(),
+                )
+                    : const Text(
+                  '마지막 페이지 입니다 ㅠㅠ',
+                ),
+              );
+            }
+            // final item = snapshot.data![index]; // respository에서 바로 매핑
+            final pItem = cp.data[index];
+
+            return widget.itemBuilder(
+              context,
+              index,
+              pItem
+            );
+          },
+          separatorBuilder: (_, index) {
+            return SizedBox(
+              height: 16.0,
+            );
+          },
+          itemCount: cp.data.length + 1),
+    );
+  }
+}
+
--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -1,113 +1,29 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:zodal_minzok/common/dio/dio.dart';
-import 'package:zodal_minzok/common/model/cursor_pagination_model.dart';
-import 'package:zodal_minzok/common/utils/data_utils.dart';
-import 'package:zodal_minzok/common/utils/pagination_utils.dart';
+import 'package:zodal_minzok/common/component/pagination_list_view.dart';
 import 'package:zodal_minzok/restaurant/component/restaurant_card.dart';
-import 'package:zodal_minzok/restaurant/model/restaurant_model.dart';
-import 'package:zodal_minzok/restaurant/repository/restaurant_repository.dart';
 import 'package:zodal_minzok/restaurant/provider/restaurant_provider.dart';
 import 'package:zodal_minzok/restaurant/view/restaurant_detail_screen.dart';
 
-import '../../common/const/data.dart';
 
 // @author zosu
 // @since 2024-03-24
 // @comment 가게 리스트
-class RestaurantScreen extends ConsumerStatefulWidget {
-  const RestaurantScreen({super.key});
 
-  @override
-  _RestaurantScreenState createState() => _RestaurantScreenState();
-}
-
-class _RestaurantScreenState extends ConsumerState<RestaurantScreen> {
-  late ScrollController _controller;
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-
-    _controller = ScrollController();
-    _controller.addListener(scrollListener);
-  }
-
-  void scrollListener() {
-    // 스크롤 기능 공통화
-    PaginationUtils.paginate(controller: _controller, provider: ref
-        .watch(restaurantStateNotifierProvider.notifier),);
-  }
-
+class RestaurantScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    final data = ref.watch(restaurantStateNotifierProvider);
 
-    // 맨 처음 로딩할 때
-    if (data is CursorPaginationLoading) {
-      return Center(
-        child: CircularProgressIndicator(),
-      );
-    }
-
-    // 에러가 발생
-    if (data is CursorPaginationError) {
-      return Center(
-        child: Text(data.error),
-      );
-    }
-
-    // 아래 세가지는 모두 CursorPagination
-    // CursorPagination
-    // CursorPaginationFetchingMore
-    // CursorPaginationRefetch
-
-    // 임시
-    final cp = data as CursorPagination;
-
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 16.0,
-      ),
-      child: ListView.separated(
-          controller: _controller,
-          itemBuilder: (_, index) {
-            /// 데이터를 더 조회하고 있을 시 로딩 
-            if (index == cp.data.length) {
-              return Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 16.0,
-                ),
-                child: cp is CursorPaginationFetchingMore /// 데이터가 더이상 없을때 로딩창을 보여주면 안됨
-                    ? const Center(
-                        child: CircularProgressIndicator(),
-                      )
-                    : const Text(
-                        '마지막 페이지 입니다 ㅠㅠ',
-                      ), 
-              );
-            }
-            // final item = snapshot.data![index]; // respository에서 바로 매핑
-            final pItem = cp.data[index];
-
-            return GestureDetector(
-                onTap: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (_) => RestaurantDetailScreen(
-                            id: pItem.id,
-                          )));
-                },
-                child: RestaurantCard.fromModel(model: pItem));
+    return PaginationListView(provider: restaurantStateNotifierProvider,
+      itemBuilder: <RestaurantModel>(context, index, model) {
+      return GestureDetector(
+          onTap: () {
+            Navigator.of(context).push(MaterialPageRoute(
+                builder: (_) => RestaurantDetailScreen(
+                  id: model.id,
+                )));
           },
-          separatorBuilder: (_, index) {
-            return SizedBox(
-              height: 16.0,
-            );
-          },
-          itemCount: cp.data.length + 1),
-    );
+          child: RestaurantCard.fromModel(model: model));
+    },);
+
   }
 }
